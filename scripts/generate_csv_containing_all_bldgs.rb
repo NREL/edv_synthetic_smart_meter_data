79b3a744d92dc616f9613794fe6db5714eea9213
mrigesh <mrigesh.gupta@d-alchemy.com>
2019-09-13 03:34:28 -0700

    import actual data from bgdp as monthly timeseries in results.xml

--- scripts/generate_csv_containing_all_bldgs.rb
+++ scripts/generate_csv_containing_all_bldgs.rb
@@ -1,11 +1,9 @@
 require 'fileutils'
 
-root_dir = File.expand_path( '../', File.dirname(__FILE__))
+root_dir = File.expand_path('../', File.dirname(__FILE__))
 puts "root_dir:#{root_dir}"
 
-if !ARGV[0].nil?  && Dir.exist?(ARGV[0])
-  root_dir = ARGV[0]
-end
+root_dir = ARGV[0] if !ARGV[0].nil? && Dir.exist?(ARGV[0])
 
 standard_to_be_used = 'ASHRAE90.1'
 if !ARGV[1].nil? && (ARGV[1] == 'CaliforniaTitle24' || ARGV[1] == 'ASHRAE90.1')
@@ -14,9 +12,7 @@ end
 puts "standard_to_be_used:#{standard_to_be_used}"
 
 epw_file = 'temporary.epw'
-if !ARGV[2].nil? && File.exist?(ARGV[2])
-  epw_file = ARGV[2]
-end
+epw_file = ARGV[2] if !ARGV[2].nil? && File.exist?(ARGV[2])
 puts "epw_file:#{epw_file}"
 
 csv_file_path = File.expand_path('../spec/output/csv_files/all.csv', File.dirname(__FILE__))
