bf6822929075a64780af8d84d58314d4693db0e5
Kim <jkim4@nrel.gov>
2020-05-07 09:45:48 -0600

    Update simulate_bdgp_xml_path.rb
    adding functions for occ var

--- scripts/helper/simulate_bdgp_xml_path.rb
+++ scripts/helper/simulate_bdgp_xml_path.rb
@@ -3,6 +3,7 @@ require 'openstudio/model_articulation/os_lib_model_generation_bricr'
 require 'buildingsync'
 require 'buildingsync/translator'
 require_relative '../constants'
+require 'openstudio-occupant-variability'
 
 
 def simulate_bdgp_xml_path(xml_file_path, standard, epw_file_path, ddy_file_path, baseline_only)
@@ -20,6 +21,17 @@ def simulate_bdgp_xml_path(xml_file_path, standard, epw_file_path, ddy_file_path
     translator.add_measure_path("#{root_dir}/lib/measures")
     translator.insert_reporting_measure('hourly_consumption_by_fuel_to_csv', 0)
     translator.write_osm(ddy_file_path)
+    ###################################################################################
+    # Comment this block, if simulating without Occupant Variability
+    ###################################################################################
+    occupant_variability_instance = OpenStudio::OccupantVariability::Extension.new
+    translator.add_measure_path(occupant_variability_instance.measures_dir)
+    translator.insert_energyplus_measure('Occupancy_Simulator', 0)
+    translator.insert_energyplus_measure('create_lighting_schedule_from_occupant_count', 0)
+    translator.insert_energyplus_measure('create_mels_schedule_from_occupant_count', 0)
+    translator.insert_energyplus_measure('update_hvac_setpoint_schedule', 0)
+    ###################################################################################
+    
     translator.write_osws
 
     translator.run_osws(baseline_only)
