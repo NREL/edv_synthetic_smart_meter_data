afb887d1dd9d1c6bebba1288e2cbe980e4667c39
Janghyun(JK)Kim <kimjanghyun84@gmail.com>
2020-08-04 16:50:49 -0600

    mapping building location added for BDGP2
    - referred existing map_latlng.rb script
    - to read lat/lng and populate zipcode/city/state information as separate columns

--- scripts/helper/standardize_input.rb
+++ scripts/helper/standardize_input.rb
@@ -1,37 +1,111 @@
 class StandardizedInput
 
-  def copy_columns(file, std_labels, outdir, options = {headers: true, header_converters: :symbol})
-    unless File.exists?(outdir + '/metadata.csv')
-    
-      metadata_file = File.open(outdir + '/metadata.csv', 'w')
-      metadata_file.puts std_labels
+  def map_location_with_latlng(file, outdir, options = {headers: true, header_converters: :symbol})
+    updated_features = []
+    headers = []
+    lat_lng_arr = []
 
-      CSV.foreach(file, options) do |feature|
+    options = {headers:true, header_converters: :symbol}
 
-        building_id = feature[:uid]
-        primary_building_type = feature[:primaryspaceusage]
-        floor_area_sqft = feature[:sqft]
-        vintage = feature[:yearbuilt]
-        climate_zone = feature[:climate_zone]
-        weather_file_name_epw = feature[:epw]
-        weather_file_name_ddy = feature[:ddy]
-        zipcode = feature[:zipcode]
-        city = feature[:city]
-        us_state = feature[:state]
-        longitude = feature[:lng]
-        latitude = feature[:lat]
-        number_of_stories = feature[:numberoffloors]
-        number_of_occupants = feature[:occupants]
-        fuel_type_heating = feature[:heatingtype]
-        energystar_score = feature[:energystarscore]
-        measurement_start_date = feature[:datastart]
-        measurement_end_date = feature[:dataend]
-        
-        metadata_file.puts "#{building_id},#{building_id}.xml,#{primary_building_type},#{floor_area_sqft},#{vintage},#{climate_zone},#{zipcode},#{city},#{us_state},#{longitude},#{latitude},#{number_of_stories},#{number_of_occupants},#{fuel_type_heating},#{energystar_score},#{measurement_start_date},#{measurement_end_date},#{weather_file_name_epw},#{weather_file_name_ddy}"
-        
-      end
-      
+    CSV.foreach(file, options) do |row|
+	  the_hash = {}
+	  the_hash[:building_id] = row[:building_id]
+	  the_hash[:lat] = row[:lat]
+	  the_hash[:lng] = row[:lng]
+	  lat_lng_arr << the_hash
     end
+
+    # puts "lat_lng_arr size: #{lat_lng_arr.size}"
+
+    i=0
+    puts "###############################################"
+    CSV.foreach(file, options) do |feature|
+      i=i+1
+	  puts "Verifying latitude and longitude in location #{i} out of #{lat_lng_arr.size} locations"
+	  headers = feature.headers
+	  id = feature[:building_id]
+
+	  matches = lat_lng_arr.select {|row| row[:building_id] === id }
+	  # puts "num matches: #{matches.size}"
+	  if matches.size > 0
+		the_match = matches[0]
+		feature[:lat] = the_match[:lat]
+		feature[:lng] = the_match[:lng]
+
+		# geocoder
+		res = Geocoder.search("#{feature[:lat]}, #{feature[:lng]}")
+		if (feature[:lat].to_s.empty?)||(feature[:lng].to_s.empty?)
+		  feature[:zipcode] = ""
+		  puts "zipcode: n/a"
+		  feature[:city] = ""
+		  puts "city: n/a"
+		  feature[:state] = ""
+		  puts "state: n/a"
+		else
+		  feature[:zipcode] = res.first.postal_code
+		  puts "zipcode: #{res.first.postal_code}"
+		  feature[:city] = res.first.city
+		  puts "city: #{res.first.city}"
+		  feature[:state] = res.first.state
+		  puts "state: #{res.first.state}"
+		end
+
+	  end
+	  puts "###############################################"
+
+	  updated_features << feature
+	
+    end
+
+    # headers << :zipcode
+    # headers << :city
+    # headers << :state
+
+    # # write new array to file
+    # puts "Processed file saved at #{outdir}"
+    # CSV.open(outdir + "/metadata.csv", "w+") do |csv|
+      # csv << headers
+      # updated_features.each do |row|
+  	    # csv << row
+      # end
+    # end
+	
+	return updated_features
+		
+  end
+
+  def copy_columns(file, std_labels, outdir, updated_features, options = {headers: true, header_converters: :symbol})
+    metadata_file = File.open(outdir + '/metadata.csv', 'w+')
+	
+	puts "Adding standard labels (header names) into metadata.csv file"
+    metadata_file.puts std_labels
+		  
+	puts "Adding values of standard labels into metadata.csv file"
+	updated_features.each do |row|
+		
+	  building_id = row[:building_id]
+      primary_building_type = row[:primaryspaceusage]
+      floor_area_sqft = row[:sqft]
+      vintage = row[:yearbuilt]
+      climate_zone = row[:climate_zone]
+      weather_file_name_epw = row[:epw]
+      weather_file_name_ddy = row[:ddy]
+      zipcode = row[:zipcode]
+      city = row[:city]
+      us_state = row[:state]
+      longitude = row[:lng]
+      latitude = row[:lat]
+      number_of_stories = row[:numberoffloors]
+      number_of_occupants = row[:occupants]
+      fuel_type_heating = row[:heatingtype]
+      energystar_score = row[:energystarscore]
+      measurement_start_date = row[:datastart]
+      measurement_end_date = row[:dataend]
+		
+	  metadata_file.puts "#{building_id},#{building_id}.xml,#{primary_building_type},#{floor_area_sqft},#{vintage},#{climate_zone},#{zipcode},#{city},#{us_state},#{longitude},#{latitude},#{number_of_stories},#{number_of_occupants},#{fuel_type_heating},#{energystar_score},#{measurement_start_date},#{measurement_end_date},#{weather_file_name_epw},#{weather_file_name_ddy}"
+
+    end
+	puts "###############################################"
     
   end
   
--- scripts/rawdata_to_standardized_input_BDGP.rb
+++ scripts/rawdata_to_standardized_input_BDGP.rb
@@ -4,6 +4,7 @@ require 'csv'
 require 'fileutils'
 require_relative 'constants'
 require_relative 'helper/standardize_input'
+require 'geocoder'
 
 if ARGV[0].nil? || !File.exist?(ARGV[0])
   puts 'Error - No metadata CSV file specified'
@@ -22,14 +23,18 @@ outdir = "./data/processed"
 FileUtils.mkdir_p(outdir) unless File.exist?(outdir)
 
 #######################################################################
+# map building location (zipcode/city/state) based on latitude and longitude
+#######################################################################
+
+updated_features = StandardizedInput.new.map_location_with_latlng(ARGV[0], outdir)
+
+#######################################################################
 # metadata data conversion from raw labels to standardized labels
 #######################################################################
 
-# std_labels = ['building_id','xml_filename','primary_building_type','floor_area_sqft','vintage','climate_zone','zipcode','city','us_state','longitude','latitude','number_of_stories','number_of_occupants','fuel_type_heating','energystar_score','measurement_start_date','measurement_end_date','weather_file_name_epw','weather_file_name_ddy']
-
 std_labels = 'building_id,xml_filename,primary_building_type,floor_area_sqft,vintage,climate_zone,zipcode,city,us_state,longitude,latitude,number_of_stories,number_of_occupants,fuel_type_heating,energystar_score,measurement_start_date,measurement_end_date,weather_file_name_epw,weather_file_name_ddy'
 
-StandardizedInput.new.copy_columns(ARGV[0], std_labels, outdir)
+StandardizedInput.new.copy_columns(ARGV[0], std_labels, outdir, updated_features)
 
 #######################################################################
 # timeseries data conversion from raw labels to standardized labels
@@ -39,7 +44,8 @@ data_original = CSV.read(ARGV[1])
 header = CSV.open(ARGV[1], &:readline)
 #header[header.index("timestamp")] = "timestamp"
 
+puts "Copying timeseries data into timeseriesdata.csv file"
 CSV.open(outdir + '/timeseriesdata.csv', "w", :headers => true) do |csv|
   csv << header
-  data_original.each_with_index {|row,i| next if i == 0; csv << row } #HOW TO SKIP FIRST ROW
+  data_original.each_with_index {|row,i| next if i == 0; csv << row }
 end
