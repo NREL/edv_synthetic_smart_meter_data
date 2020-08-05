949f18312d4f5ff1e9f11b353a594c8aeb5fc8a6
Janghyun(JK)Kim <kimjanghyun84@gmail.com>
2020-08-05 13:14:08 -0600

    simulation control file updated
    - skipping weather file searching when weather file names are not specified in the metadata
    - made changes in jupyter notebook (https://github.com/NREL/edv-experiment-1-files/blob/master/map_weather_file.ipynb) to align the process with updated worflow

--- scripts/generate_csv_containing_all_bldgs.rb
+++ scripts/generate_csv_containing_all_bldgs.rb
@@ -6,11 +6,13 @@ root_dir = File.expand_path('../', File.dirname(__FILE__))
 
 root_dir = ARGV[0] if !ARGV[0].nil? && Dir.exist?(ARGV[0])
 
+puts "###############################################"
+
 standard_to_be_used = 'ASHRAE90.1'
 if !ARGV[1].nil? && (ARGV[1] == 'CaliforniaTitle24' || ARGV[1] == 'ASHRAE90.1')
   standard_to_be_used = ARGV[1]
 end
-puts "standard_to_be_used:#{standard_to_be_used}"
+puts "standard_to_be_used: #{standard_to_be_used}"
 
 epw_file = 'temporary.epw'
 ddy_file = 'temporary.ddy'
@@ -39,19 +41,33 @@ csv_file_path = File.expand_path("../#{NAME_OF_OUTPUT_DIR}/Control_Files/all.csv
 FileUtils.mkdir_p File.dirname(csv_file_path)
 puts "csv_file_path: #{csv_file_path}"
 
+puts "###############################################"
+
 csv = File.open(csv_file_path, 'w')
+totalcount = Dir.glob("#{root_dir}/*.xml").count
 puts "Looking for #{"#{root_dir}/*.xml"} "
-puts "found #{Dir.glob("#{root_dir}/*.xml").count} xml files in this directory. "
+puts "Found #{totalcount} xml files in this directory. "
+
+puts "###############################################"
+puts "Adding epw file path/name and ddy file path/name in the control file."
+i=0
 Dir.glob("#{root_dir}/*.xml").each do |xml_file|
+  i=i+1
   matches = epw_arr.select {|row| row[:building_id] === File.basename(xml_file, ".xml") }
+  
+  puts "Matching building #{i} from #{totalcount} buildings: #{matches}"
+  
   if matches.size > 0
-    epw_file = File.expand_path(matches[0][:weather_file_name_epw], weather_file_source_dir)
-    ddy_file = File.expand_path(matches[0][:weather_file_name_ddy], weather_file_source_dir)
+    if (!matches[0][:weather_file_name_epw].nil?)||(!matches[0][:weather_file_name_ddy].nil?)
+      epw_file = File.expand_path(matches[0][:weather_file_name_epw], weather_file_source_dir)
+      ddy_file = File.expand_path(matches[0][:weather_file_name_ddy], weather_file_source_dir)
+    end
   end
   # puts " epw: #{epw_file} ddy: #{ddy_file}"
   csv.puts("#{File.basename(xml_file)},#{standard_to_be_used},#{epw_file},#{ddy_file}")
   csv.flush
 end
+puts "###############################################"
 csv.close
 
 
