42a6b0158a4b8b29bf1742050ac28b0d5befc96f
lin <llin@nrel.gov>
2020-10-05 09:22:03 -0600

    calculate_metrics cosmetics

--- scripts/calculate_metrics.rb
+++ scripts/calculate_metrics.rb
@@ -35,7 +35,7 @@ def get_floor_area_value(doc, ns)
   measured_floor_element = nil
   floor_areas = doc.elements["/#{ns}:BuildingSync/#{ns}:Facilities/#{ns}:Facility/#{ns}:Sites/#{ns}:Site/#{ns}:Buildings/#{ns}:Building/#{ns}:FloorAreas"]
   floor_areas.each do |floor_element|
-    next if floor_element.class == REXML::Text
+    next if floor_element.class != REXML::Element
     begin
       if floor_element.elements["#{ns}:FloorAreaType"].text == 'Gross'
         measured_floor_element = floor_element
@@ -53,12 +53,9 @@ def read_time_series_data(scenario_element, ns, resource_use_id = nil)
   time_series_data = scenario_element.elements["#{ns}:TimeSeriesData"]
   time_series_data.each do |time_series|
     if resource_use_id.nil? || time_series.elements["#{ns}:ResourceUseID"].attributes['IDref'] == resource_use_id
-      next if time_series.class == REXML::Text
+      next if time_series.class != REXML::Element
       datetime = time_series.elements["#{ns}:StartTimestamp"].text
       monthly_data.add_start_date_string(datetime)
-      #monthly_measured_data.update_year(datetime.year)
-      #monthly_measured_data.update_month(datetime.month)
-      #monthly_measured_data.update_end_time(time_series.elements["#{ns}:EndTimestamp"].text)
       monthly_data.update_total_values(time_series.elements["#{ns}:IntervalReading"].text, counter)
       counter += 1
     end
@@ -67,7 +64,6 @@ def read_time_series_data(scenario_element, ns, resource_use_id = nil)
 end
 
 Dir.glob(File.join(indir, "/*.xml")).each do |xml_file_path|
-  # open the xml file
   doc = nil
   File.open(xml_file_path, 'r') do |xml_file_path|
     doc = REXML::Document.new(xml_file_path)
@@ -79,23 +75,15 @@ Dir.glob(File.join(indir, "/*.xml")).each do |xml_file_path|
 
     floor_area = get_floor_area_value(doc, ns)
     monthly_measured_data = nil
-    # first we get the measured scenario
     scenario_elements = doc.elements["/#{ns}:BuildingSync/#{ns}:Facilities/#{ns}:Facility/#{ns}:Reports/#{ns}:Report/#{ns}:Scenarios"]
-    # for the first pass we just look for the measured scenario
     scenario_elements.each do |scenario_element|
-      next if scenario_element.class == REXML::Text
+      next if scenario_element.class != REXML::Element
       if scenario_element.attributes['ID'] == 'Measured'
         monthly_measured_data = read_time_series_data(scenario_element, ns)
 
         eui = MetricsCalc.calculate_eui_value(monthly_measured_data.get_summary, floor_area)
         eui_mea_count += MetricsCalc.add_eui(scenario_element, eui, ns)
-      end
-    end
-
-    # the second pass we look for simulated scenarios
-    scenario_elements.each do |scenario_element|
-      next if scenario_element.class == REXML::Text
-      if scenario_element.attributes['ID'] != 'Measured'
+      else
         electricity_resource_use_id = get_electric_resource_use_id(scenario_element, ns)
         if electricity_resource_use_id
 
@@ -114,7 +102,6 @@ Dir.glob(File.join(indir, "/*.xml")).each do |xml_file_path|
     end
 
     new_xml_file_path = File.absolute_path(xml_file_path).gsub("#{SIM_FILES_DIR}", "#{CALC_METRICS_DIR}")
-    # save the file as
     unless Dir.exist?(File.dirname(new_xml_file_path))
       FileUtils.mkdir_p(File.dirname(new_xml_file_path))
     end
@@ -122,7 +109,8 @@ Dir.glob(File.join(indir, "/*.xml")).each do |xml_file_path|
     File.open(new_xml_file_path, 'w') do |file|
       doc.write(file)
     end
-
-    puts "successfully added the following metrics: Measured EUI: #{eui_mea_count}, Simulated EUI: #{eui_sim_count}, CVRMSE: #{cvrmse_count} and NMBE: #{nmbe_count} to file #{new_xml_file_path}"
+    if !eui_mea_count.zero? || !eui_sim_count.zero? || !cvrmse_count.zero? || !nmbe_count.zero?
+      puts "successfully added the following metrics: Measured EUI: #{eui_mea_count}, Simulated EUI: #{eui_sim_count}, CVRMSE: #{cvrmse_count} and NMBE: #{nmbe_count} to file #{File.basename(new_xml_file_path)}"
+    end
   end
 end
