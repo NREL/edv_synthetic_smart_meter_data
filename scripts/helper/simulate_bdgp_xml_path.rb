d5f0870cfdfde156f1814e0c0ce479b6938d6e77
lin <llin@nrel.gov>
2021-09-07 15:24:27 -0600

    simulate_bdgp_xml_path measure path

--- scripts/helper/simulate_bdgp_xml_path.rb
+++ scripts/helper/simulate_bdgp_xml_path.rb
@@ -19,14 +19,15 @@ def simulate_bdgp_xml_path(xml_file_path, standard, epw_file_path, ddy_file_path
   out_xml = File.basename(xml_file_path)
   root_dir = File.expand_path('../..', File.dirname(__FILE__))
 
-  begin
+  # begin
     translator = BuildingSync::Translator.new(xml_file_path, out_path, epw_file_path, standard, false)
     facility = translator.get_facility
     facility.add_cb_modeled('Baseline')
 
-    measure_dir = File.join(root_dir, 'lib', 'measures', 'hourly_consumption_by_fuel_to_csv')
-    translator.add_measure_path(measure_dir)
-    translator.insert_measure_into_workflow('ReportingMeasure', measure_dir, 0, nil)
+    measure_dir = 'hourly_consumption_by_fuel_to_csv'
+    measure_path = File.join(root_dir, 'lib', 'measures')
+    translator.add_measure_path(measure_path)
+    translator.insert_measure_into_workflow('ReportingMeasure', measure_dir, 0, {})
     translator.setup_and_sizing_run
 
     ###########################################################################################
@@ -46,10 +47,10 @@ def simulate_bdgp_xml_path(xml_file_path, standard, epw_file_path, ddy_file_path
     if occ_var
       occupant_variability_instance = OpenStudio::OccupantVariability::Extension.new
       translator.add_measure_path(occupant_variability_instance.measures_dir)
-      translator.insert_measure_into_workflow('ModelMeasure', 'Occupancy_Simulator_os', 0, nil)
-      translator.insert_measure_into_workflow('ModelMeasure', 'create_lighting_schedule_from_occupant_count', 0, nil)
-      translator.insert_measure_into_workflow('ModelMeasure', 'create_mels_schedule_from_occupant_count', 0, nil)
-      translator.insert_measure_into_workflow('ModelMeasure', 'update_hvac_setpoint_schedule', 0, nil)
+      translator.insert_measure_into_workflow('ModelMeasure', 'Occupancy_Simulator_os', 0, {})
+      translator.insert_measure_into_workflow('ModelMeasure', 'create_lighting_schedule_from_occupant_count', 0, {})
+      translator.insert_measure_into_workflow('ModelMeasure', 'create_mels_schedule_from_occupant_count', 0, {})
+      translator.insert_measure_into_workflow('ModelMeasure', 'update_hvac_setpoint_schedule', 0, {})
     end
 
     unless non_routine_var.empty?
@@ -58,72 +59,74 @@ def simulate_bdgp_xml_path(xml_file_path, standard, epw_file_path, ddy_file_path
 
       ## 1. Demand response measures
       if NON_ROUTINE_VAR[:DR_GTA_os]
-        translator.insert_measure_into_workflow('ModelMeasure', 'DR_GTA_os', 0, nil)
+        translator.insert_measure_into_workflow('ModelMeasure', 'DR_GTA_os', 0, {})
       end
       if NON_ROUTINE_VAR[:DR_Lighting_os]
-        translator.insert_measure_into_workflow('ModelMeasure', 'DR_Lighting_os', 0, nil)
+        translator.insert_measure_into_workflow('ModelMeasure', 'DR_Lighting_os', 0, {})
       end
       if NON_ROUTINE_VAR[:DR_MELs_os]
-        translator.insert_measure_into_workflow('ModelMeasure', 'DR_MELs_os', 0, nil)
+        translator.insert_measure_into_workflow('ModelMeasure', 'DR_MELs_os', 0, {})
       end
       if NON_ROUTINE_VAR[:DR_Precool_Preheat_os]
-        translator.insert_measure_into_workflow('ModelMeasure', 'DR_Precool_Preheat_os', 0, nil)
+        translator.insert_measure_into_workflow('ModelMeasure', 'DR_Precool_Preheat_os', 0, {})
       end
 =begin
       ## 2. Faulty operation measures
       if NON_ROUTINE_VAR[:Fault_AirHandlingUnitFanMotorDegradation_ep]
-        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_AirHandlingUnitFanMotorDegradation_ep', 0, nil)
+        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_AirHandlingUnitFanMotorDegradation_ep', 0, {})
       end
       if NON_ROUTINE_VAR[:Fault_BiasedEconomizerSensorMixedT_ep]
-        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_BiasedEconomizerSensorMixedT_ep', 0, nil)
+        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_BiasedEconomizerSensorMixedT_ep', 0, {})
       end
       if NON_ROUTINE_VAR[:Fault_BiasedEconomizerSensorOutdoorRH_ep]
-        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_BiasedEconomizerSensorOutdoorRH_ep', 0, nil)
+        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_BiasedEconomizerSensorOutdoorRH_ep', 0, {})
       end
       if NON_ROUTINE_VAR[:Fault_BiasedEconomizerSensorOutdoorT_ep]
-        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_BiasedEconomizerSensorOutdoorT_ep', 0, nil)
+        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_BiasedEconomizerSensorOutdoorT_ep', 0, {})
       end
       if NON_ROUTINE_VAR[:Fault_BiasedEconomizerSensorReturnRH_ep]
-        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_BiasedEconomizerSensorReturnRH_ep', 0, nil)
+        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_BiasedEconomizerSensorReturnRH_ep', 0, {})
       end
       if NON_ROUTINE_VAR[:Fault_BiasedEconomizerSensorReturnT_ep]
-        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_BiasedEconomizerSensorReturnT_ep', 0, nil)
+        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_BiasedEconomizerSensorReturnT_ep', 0, {})
       end
       if NON_ROUTINE_VAR[:Fault_DuctFouling_os]
-        translator.insert_measure_into_workflow('ModelMeasure', 'Fault_DuctFouling_os', 0, nil)
+        translator.insert_measure_into_workflow('ModelMeasure', 'Fault_DuctFouling_os', 0, {})
       end
       if NON_ROUTINE_VAR[:Fault_EconomizerOpeningStuck_os]
-        translator.insert_measure_into_workflow('ModelMeasure', 'Fault_EconomizerOpeningStuck_os', 0, nil)
+        translator.insert_measure_into_workflow('ModelMeasure', 'Fault_EconomizerOpeningStuck_os', 0, {})
       end
       if NON_ROUTINE_VAR[:Fault_ExcessiveInfiltration_os]
-        translator.insert_measure_into_workflow('ModelMeasure', 'Fault_ExcessiveInfiltration_os', 0, nil)
+        translator.insert_measure_into_workflow('ModelMeasure', 'Fault_ExcessiveInfiltration_os', 0, {})
       end
       if NON_ROUTINE_VAR[:Fault_HVACSetbackErrorDelayedOnset_os]
-        translator.insert_measure_into_workflow('ModelMeasure', 'Fault_HVACSetbackErrorDelayedOnset_os', 0, nil)
+        translator.insert_measure_into_workflow('ModelMeasure', 'Fault_HVACSetbackErrorDelayedOnset_os', 0, {})
       end
       if NON_ROUTINE_VAR[:Fault_HVACSetbackErrorEarlyTermination_os]
-        translator.insert_measure_into_workflow('ModelMeasure', 'Fault_HVACSetbackErrorEarlyTermination_os', 0, nil)
+        translator.insert_measure_into_workflow('ModelMeasure', 'Fault_HVACSetbackErrorEarlyTermination_os', 0, {})
       end
       if NON_ROUTINE_VAR[:Fault_HVACSetbackErrorNoOvernightSetback_os]
-        translator.insert_measure_into_workflow('ModelMeasure', 'Fault_HVACSetbackErrorNoOvernightSetback_os', 0, nil)
+        translator.insert_measure_into_workflow('ModelMeasure', 'Fault_HVACSetbackErrorNoOvernightSetback_os', 0, {})
       end
       if NON_ROUTINE_VAR[:Fault_thermostat_offset_ep]
-        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_thermostat_offset_ep', 0, nil)
+        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_thermostat_offset_ep', 0, {})
       end
 =end
+
       ## 3. Retrofit measures
       if NON_ROUTINE_VAR[:Retrofit_equipment_os]
-        translator.insert_measure_into_workflow('ModelMeasure', 'Retrofit_equipment_os', 0, nil)
+        translator.insert_measure_into_workflow('ModelMeasure', 'Retrofit_equipment_os', 0, {})
       end
       if NON_ROUTINE_VAR[:Retrofit_lighting_os]
-        translator.insert_measure_into_workflow('ModelMeasure', 'Retrofit_lighting_os', 0, nil)
+        translator.insert_measure_into_workflow('ModelMeasure', 'Retrofit_lighting_os', 0, {})
       end
       if NON_ROUTINE_VAR[:Retrofit_exterior_wall_ep]
-        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Retrofit_exterior_wall_ep', 0, nil)
+        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Retrofit_exterior_wall_ep', 0, {})
       end
       if NON_ROUTINE_VAR[:Retrofit_roof_ep]
-        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Retrofit_roof_ep', 0, nil)
+        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Retrofit_roof_ep', 0, {})
       end
+
     end
 
     translator.write_osws
@@ -131,7 +134,8 @@ def simulate_bdgp_xml_path(xml_file_path, standard, epw_file_path, ddy_file_path
     translator.gather_results(m2.assumedYear, BASELINE_ONLY)
     translator.prepare_final_xml
     translator.save_xml(out_xml)
-  rescue StandardError => e
-    puts "Error occurred while processing #{xml_file_path} with message: #{e.message}"
-  end
+
+  # rescue StandardError => e
+  #   puts "Error occurred while processing #{xml_file_path} with message: #{e.message}"
+  # end
 end
