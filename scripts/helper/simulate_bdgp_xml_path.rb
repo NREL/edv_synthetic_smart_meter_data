8406c8f35e218f15bbbc4a6fa9db33e2325178cc
Janghyun(JK)Kim <janghyun.kim@nrel.gov>
2021-12-23 15:24:04 -0700

    enabling LBNL gems

--- scripts/helper/simulate_bdgp_xml_path.rb
+++ scripts/helper/simulate_bdgp_xml_path.rb
@@ -3,8 +3,8 @@ require 'openstudio/extension'
 require 'openstudio/model_articulation'
 require 'buildingsync'
 require 'buildingsync/translator'
-#require 'openstudio/occupant_variability'
-#require 'openstudio-variability'
+require 'openstudio/occupant_variability'
+require 'openstudio-variability'
 require 'rexml/document'
 
 require_relative '../constants'
@@ -54,89 +54,89 @@ def simulate_bdgp_xml_path(xml_file_path, standard, epw_file_path, ddy_file_path
     puts "Calendar year set to: #{m2.assumedYear}"
     ###########################################################################################
 
-    # if occ_var
-    #   occupant_variability_instance = OpenStudio::OccupantVariability::Extension.new
-    #   translator.add_measure_path(occupant_variability_instance.measures_dir)
-    #   translator.insert_measure_into_workflow('ModelMeasure', 'Occupancy_Simulator_os', 0, {})
-    #   translator.insert_measure_into_workflow('ModelMeasure', 'create_lighting_schedule_from_occupant_count', 0, {})
-    #   translator.insert_measure_into_workflow('ModelMeasure', 'create_mels_schedule_from_occupant_count', 0, {})
-    #   translator.insert_measure_into_workflow('ModelMeasure', 'update_hvac_setpoint_schedule', 0, {})
-    # end
+    if occ_var
+      occupant_variability_instance = OpenStudio::OccupantVariability::Extension.new
+      translator.add_measure_path(occupant_variability_instance.measures_dir)
+      translator.insert_measure_into_workflow('ModelMeasure', 'Occupancy_Simulator_os', 0, {})
+      translator.insert_measure_into_workflow('ModelMeasure', 'create_lighting_schedule_from_occupant_count', 0, {})
+      translator.insert_measure_into_workflow('ModelMeasure', 'create_mels_schedule_from_occupant_count', 0, {})
+      translator.insert_measure_into_workflow('ModelMeasure', 'update_hvac_setpoint_schedule', 0, {})
+    end
 
-    # unless non_routine_var.empty?
-    #   variability_instance = OpenStudio::Variability::Extension.new
-    #   translator.add_measure_path(variability_instance.measures_dir)
+    unless non_routine_var.empty?
+      variability_instance = OpenStudio::Variability::Extension.new
+      translator.add_measure_path(variability_instance.measures_dir)
 
-    #   ## 1. Demand response measures
-    #   if NON_ROUTINE_VAR[:DR_GTA_os]
-    #     translator.insert_measure_into_workflow('ModelMeasure', 'DR_GTA_os', 0, {})
-    #   end
-    #   if NON_ROUTINE_VAR[:DR_Lighting_os]
-    #     translator.insert_measure_into_workflow('ModelMeasure', 'DR_Lighting_os', 0, {})
-    #   end
-    #   if NON_ROUTINE_VAR[:DR_MELs_os]
-    #     translator.insert_measure_into_workflow('ModelMeasure', 'DR_MELs_os', 0, {})
-    #   end
-    #   if NON_ROUTINE_VAR[:DR_Precool_Preheat_os]
-    #     translator.insert_measure_into_workflow('ModelMeasure', 'DR_Precool_Preheat_os', 0, {})
-    #   end
+      ## 1. Demand response measures
+      if NON_ROUTINE_VAR[:DR_GTA_os]
+        translator.insert_measure_into_workflow('ModelMeasure', 'DR_GTA_os', 0, {})
+      end
+      if NON_ROUTINE_VAR[:DR_Lighting_os]
+        translator.insert_measure_into_workflow('ModelMeasure', 'DR_Lighting_os', 0, {})
+      end
+      if NON_ROUTINE_VAR[:DR_MELs_os]
+        translator.insert_measure_into_workflow('ModelMeasure', 'DR_MELs_os', 0, {})
+      end
+      if NON_ROUTINE_VAR[:DR_Precool_Preheat_os]
+        translator.insert_measure_into_workflow('ModelMeasure', 'DR_Precool_Preheat_os', 0, {})
+      end
 
-    #   ## 2. Faulty operation measures
-    #   if NON_ROUTINE_VAR[:Fault_AirHandlingUnitFanMotorDegradation_ep]
-    #     translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_AirHandlingUnitFanMotorDegradation_ep', 0, {})
-    #   end
-    #   if NON_ROUTINE_VAR[:Fault_BiasedEconomizerSensorMixedT_ep]
-    #     translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_BiasedEconomizerSensorMixedT_ep', 0, {})
-    #   end
-    #   if NON_ROUTINE_VAR[:Fault_BiasedEconomizerSensorOutdoorRH_ep]
-    #     translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_BiasedEconomizerSensorOutdoorRH_ep', 0, {})
-    #   end
-    #   if NON_ROUTINE_VAR[:Fault_BiasedEconomizerSensorOutdoorT_ep]
-    #     translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_BiasedEconomizerSensorOutdoorT_ep', 0, {})
-    #   end
-    #   if NON_ROUTINE_VAR[:Fault_BiasedEconomizerSensorReturnRH_ep]
-    #     translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_BiasedEconomizerSensorReturnRH_ep', 0, {})
-    #   end
-    #   if NON_ROUTINE_VAR[:Fault_BiasedEconomizerSensorReturnT_ep]
-    #     translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_BiasedEconomizerSensorReturnT_ep', 0, {})
-    #   end
-    #   if NON_ROUTINE_VAR[:Fault_DuctFouling_os]
-    #     translator.insert_measure_into_workflow('ModelMeasure', 'Fault_DuctFouling_os', 0, {})
-    #   end
-    #   if NON_ROUTINE_VAR[:Fault_EconomizerOpeningStuck_os]
-    #     translator.insert_measure_into_workflow('ModelMeasure', 'Fault_EconomizerOpeningStuck_os', 0, {})
-    #   end
-    #   if NON_ROUTINE_VAR[:Fault_ExcessiveInfiltration_os]
-    #     translator.insert_measure_into_workflow('ModelMeasure', 'Fault_ExcessiveInfiltration_os', 0, {})
-    #   end
-    #   if NON_ROUTINE_VAR[:Fault_HVACSetbackErrorDelayedOnset_os]
-    #     translator.insert_measure_into_workflow('ModelMeasure', 'Fault_HVACSetbackErrorDelayedOnset_os', 0, {})
-    #   end
-    #   if NON_ROUTINE_VAR[:Fault_HVACSetbackErrorEarlyTermination_os]
-    #     translator.insert_measure_into_workflow('ModelMeasure', 'Fault_HVACSetbackErrorEarlyTermination_os', 0, {})
-    #   end
-    #   if NON_ROUTINE_VAR[:Fault_HVACSetbackErrorNoOvernightSetback_os]
-    #     translator.insert_measure_into_workflow('ModelMeasure', 'Fault_HVACSetbackErrorNoOvernightSetback_os', 0, {})
-    #   end
-    #   if NON_ROUTINE_VAR[:Fault_thermostat_offset_ep]
-    #     translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_thermostat_offset_ep', 0, {})
-    #   end
+      ## 2. Faulty operation measures
+      if NON_ROUTINE_VAR[:Fault_AirHandlingUnitFanMotorDegradation_ep]
+        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_AirHandlingUnitFanMotorDegradation_ep', 0, {})
+      end
+      if NON_ROUTINE_VAR[:Fault_BiasedEconomizerSensorMixedT_ep]
+        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_BiasedEconomizerSensorMixedT_ep', 0, {})
+      end
+      if NON_ROUTINE_VAR[:Fault_BiasedEconomizerSensorOutdoorRH_ep]
+        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_BiasedEconomizerSensorOutdoorRH_ep', 0, {})
+      end
+      if NON_ROUTINE_VAR[:Fault_BiasedEconomizerSensorOutdoorT_ep]
+        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_BiasedEconomizerSensorOutdoorT_ep', 0, {})
+      end
+      if NON_ROUTINE_VAR[:Fault_BiasedEconomizerSensorReturnRH_ep]
+        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_BiasedEconomizerSensorReturnRH_ep', 0, {})
+      end
+      if NON_ROUTINE_VAR[:Fault_BiasedEconomizerSensorReturnT_ep]
+        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_BiasedEconomizerSensorReturnT_ep', 0, {})
+      end
+      if NON_ROUTINE_VAR[:Fault_DuctFouling_os]
+        translator.insert_measure_into_workflow('ModelMeasure', 'Fault_DuctFouling_os', 0, {})
+      end
+      if NON_ROUTINE_VAR[:Fault_EconomizerOpeningStuck_os]
+        translator.insert_measure_into_workflow('ModelMeasure', 'Fault_EconomizerOpeningStuck_os', 0, {})
+      end
+      if NON_ROUTINE_VAR[:Fault_ExcessiveInfiltration_os]
+        translator.insert_measure_into_workflow('ModelMeasure', 'Fault_ExcessiveInfiltration_os', 0, {})
+      end
+      if NON_ROUTINE_VAR[:Fault_HVACSetbackErrorDelayedOnset_os]
+        translator.insert_measure_into_workflow('ModelMeasure', 'Fault_HVACSetbackErrorDelayedOnset_os', 0, {})
+      end
+      if NON_ROUTINE_VAR[:Fault_HVACSetbackErrorEarlyTermination_os]
+        translator.insert_measure_into_workflow('ModelMeasure', 'Fault_HVACSetbackErrorEarlyTermination_os', 0, {})
+      end
+      if NON_ROUTINE_VAR[:Fault_HVACSetbackErrorNoOvernightSetback_os]
+        translator.insert_measure_into_workflow('ModelMeasure', 'Fault_HVACSetbackErrorNoOvernightSetback_os', 0, {})
+      end
+      if NON_ROUTINE_VAR[:Fault_thermostat_offset_ep]
+        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Fault_thermostat_offset_ep', 0, {})
+      end
 
-    #   ## 3. Retrofit measures
-    #   if NON_ROUTINE_VAR[:Retrofit_equipment_os]
-    #     translator.insert_measure_into_workflow('ModelMeasure', 'Retrofit_equipment_os', 0, {})
-    #   end
-    #   if NON_ROUTINE_VAR[:Retrofit_lighting_os]
-    #     translator.insert_measure_into_workflow('ModelMeasure', 'Retrofit_lighting_os', 0, {})
-    #   end
-    #   if NON_ROUTINE_VAR[:Retrofit_exterior_wall_ep]
-    #     translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Retrofit_exterior_wall_ep', 0, {})
-    #   end
-    #   if NON_ROUTINE_VAR[:Retrofit_roof_ep]
-    #     translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Retrofit_roof_ep', 0, {})
-    #   end
+      ## 3. Retrofit measures
+      if NON_ROUTINE_VAR[:Retrofit_equipment_os]
+        translator.insert_measure_into_workflow('ModelMeasure', 'Retrofit_equipment_os', 0, {})
+      end
+      if NON_ROUTINE_VAR[:Retrofit_lighting_os]
+        translator.insert_measure_into_workflow('ModelMeasure', 'Retrofit_lighting_os', 0, {})
+      end
+      if NON_ROUTINE_VAR[:Retrofit_exterior_wall_ep]
+        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Retrofit_exterior_wall_ep', 0, {})
+      end
+      if NON_ROUTINE_VAR[:Retrofit_roof_ep]
+        translator.insert_measure_into_workflow('EnergyPlusMeasure', 'Retrofit_roof_ep', 0, {})
+      end
 
-    # end
+    end
 
     puts "<<<<<<------DEBUGGING------>>>>>> write osw"
     translator.write_osws
